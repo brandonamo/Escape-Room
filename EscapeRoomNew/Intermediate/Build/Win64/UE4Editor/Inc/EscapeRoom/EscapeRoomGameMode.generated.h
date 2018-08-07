4eba65572dec45dd189b06a531c127030880da72
Chris Haybittle <chrislh@hotmail.co.nz>
2018-08-08 10:55:11 +1200

    Deleted old added new

--- EscapeRoomNew/Intermediate/Build/Win64/UE4Editor/Inc/EscapeRoom/EscapeRoomGameMode.generated.h
+++ EscapeRoomNew/Intermediate/Build/Win64/UE4Editor/Inc/EscapeRoom/EscapeRoomGameMode.generated.h
@@ -13,9 +13,9 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define ESCAPEROOM_EscapeRoomGameMode_generated_h
 
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS_NO_PURE_DECLS \
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAEscapeRoomGameMode(); \
 	friend struct Z_Construct_UClass_AEscapeRoomGameMode_Statics; \
@@ -24,7 +24,7 @@ public: \
 	DECLARE_SERIALIZER(AEscapeRoomGameMode)
 
 
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS \
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS \
 private: \
 	static void StaticRegisterNativesAEscapeRoomGameMode(); \
 	friend struct Z_Construct_UClass_AEscapeRoomGameMode_Statics; \
@@ -33,7 +33,7 @@ public: \
 	DECLARE_SERIALIZER(AEscapeRoomGameMode)
 
 
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_STANDARD_CONSTRUCTORS \
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	ESCAPEROOM_API AEscapeRoomGameMode(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEscapeRoomGameMode) \
@@ -46,7 +46,7 @@ private: \
 public:
 
 
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_ENHANCED_CONSTRUCTORS \
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	ESCAPEROOM_API AEscapeRoomGameMode(AEscapeRoomGameMode&&); \
@@ -57,32 +57,32 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEscapeRoomGameMode); \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEscapeRoomGameMode)
 
 
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_PRIVATE_PROPERTY_OFFSET
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_9_PROLOG
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_GENERATED_BODY_LEGACY \
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_9_PROLOG
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_STANDARD_CONSTRUCTORS \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_GENERATED_BODY \
+#define EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS_NO_PURE_DECLS \
-	EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h_12_ENHANCED_CONSTRUCTORS \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h_12_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID EscapeRoom_Source_EscapeRoom_EscapeRoomGameMode_h
+#define CURRENT_FILE_ID EscapeRoomNew_Source_EscapeRoom_EscapeRoomGameMode_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
